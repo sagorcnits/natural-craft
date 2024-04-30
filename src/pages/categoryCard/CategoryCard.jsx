9e6b84e8218c6d116f92092758f9d8858bfdec24
Sagor Hossain <sagor.cnits@gmail.com>
2024-04-30 20:25:33 +0600

    category page added

--- src/pages/categoryCard/CategoryCard.jsx
+++ src/pages/categoryCard/CategoryCard.jsx
@@ -6,13 +6,11 @@ const CategoryCard = () => {
   return (
     <div className="max-w-6xl mx-auto">
       <h1 className="text-center text-[30px] md:text-[40px] poppins-font font-bold py-3">
-        All Wodden <span className="text-orange">Craft</span>
+        Categories <span className="text-orange">Item</span>
       </h1>
       <div className="divider-text"></div>
       <p className="poppins-paragraph text-center py-4">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, a!
-        Distinctio alias, <br /> delectus debitis in nesciunt optio impedit,
-        quasi perspiciatis animi commodi
+        You can see all the items in our category by visiting the details
       </p>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
         {categoryData.map((item, id) => (
